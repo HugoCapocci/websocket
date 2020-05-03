200d03e984879ec8eaf16957057d927241a8986b
Hugo CAPOCCI <h.capocci@gmail.com>
2020-05-03 23:51:21 +0200

    step 1 implemented

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,7 @@
 const WebSocket = require('ws');
+
+const webSocketServer = new WebSocket.Server({ port: process.env.WS_PORT });
+
+webSocketServer.on('connection', webSocket => {
+    console.log('connection established');
+});
