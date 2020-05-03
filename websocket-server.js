173d82e807912097a9f8b25cc4af2d05ab80f678
Hugo CAPOCCI <h.capocci@gmail.com>
2020-05-04 00:29:24 +0200

    step 3 implemented

--- websocket-server.js
+++ websocket-server.js
@@ -22,11 +22,3 @@ webSocketServer.on('connection', webSocket => {
 webSocketServer.on('error', (error) => {
     console.log('error occured ', error);
 });
- 
-webSocketServer.on('error', (error) => {
-    console.log('error occured ', error);
-});
-
-webSocketServer.on('close', function close() {
-    console.log('connection closed');
-});
