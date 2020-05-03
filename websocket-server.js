94f797f619807942af132fdcd0b33372cee73604
Hugo CAPOCCI <h.capocci@gmail.com>
2020-05-04 00:17:21 +0200

    step 2 implemented

--- websocket-server.js
+++ websocket-server.js
@@ -4,4 +4,12 @@ const webSocketServer = new WebSocket.Server({ port: process.env.WS_PORT });
 
 webSocketServer.on('connection', webSocket => {
     console.log('connection established');
+
+    webSocket.on('close', () => {
+        console.log('connection closed by client');
+    });
+});
+ 
+webSocketServer.on('error', (error) => {
+    console.log('error occured ', error);
 });
